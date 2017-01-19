453d03bb16480b8acd29e3751fb81ae5664a62d1
16052970 <16052970@NBC0D01J0ITSQA.corp.bankbtpn.co.id>
2017-01-19 16:34:05 +0700

    refactore enum Nationality with flags

--- src/java/eu/sig/training/ch03/Nationality.java
+++ src/java/eu/sig/training/ch03/Nationality.java
@@ -1,5 +1,20 @@
 package eu.sig.training.ch03;
 
+import java.awt.Color;
+import java.util.Arrays;
+
 public enum Nationality {
-    DUTCH, GERMAN, BELGIAN, LUXEMBOURGER, FRENCH, ITALIAN, ROMANIA, IRELAND, HUNGARIAN, BULGARIAN, RUSSIA, UNCLASSIFIED
+    DUTCH(Color.RED, Color.WHITE, Color.BLUE), GERMAN(Color.BLACK, Color.RED, Color.YELLOW),  BELGIAN(Color.BLACK, Color.YELLOW, Color.RED), 
+    FRENCH(Color.BLUE, Color.WHITE, Color.RED), ITALIAN(Color.GREEN, Color.WHITE, Color.RED), 
+    ROMANIA(Color.BLUE, Color.YELLOW, Color.RED), IRELAND(Color.GREEN, Color.WHITE, Color.ORANGE), HUNGARIAN(Color.RED, Color.WHITE, Color.GREEN), 
+    BULGARIAN(Color.WHITE, Color.GREEN, Color.RED) RUSSIA(Color.WHITE, Color.BLUE, Color.RED), UNCLASSIFIED(Color.gray);
+    
+    private Color[] flagColors; 
+    Nationality(Color[] pColor){
+        this.flagColors = pColor;
+    }
+
+    public List<Color> getFlagColors(){
+        return  Arrays.asList(this.flagColors);
+    }
 }
