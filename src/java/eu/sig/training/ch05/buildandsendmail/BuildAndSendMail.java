fe4436e3ace1cd3ce67248f9bdd15bb650d8069f
16052970 <16052970@NBC0D01J0ITSQA.corp.bankbtpn.co.id>
2017-01-20 09:55:56 +0700

    refactore params to object

--- src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
+++ src/java/eu/sig/training/ch05/buildandsendmail/BuildAndSendMail.java
@@ -2,17 +2,15 @@ package eu.sig.training.ch05.buildandsendmail;
 
 public class BuildAndSendMail {
     // tag::buildAndSendMail[]
-    public void buildAndSendMail(MailMan m, String firstName, String lastName,
-        String division, String subject, MailFont font, String message1,
-        String message2, String message3) {
+    public void buildAndSendMail(MailMan m, MailInfo mailInfo) {
         // Format the email address
-        String mId = firstName.charAt(0) + "." + lastName.substring(0, 7) + "@"
-            + division.substring(0, 5) + ".compa.ny";
+        String mId = mailInfo.getFirstName.charAt(0) + "." + mailInfo.getLastName.substring(0, 7) + "@"
+            + mailInfo.getDivision.substring(0, 5) + ".compa.ny";
         // Format the message given the content type and raw message
-        MailMessage mMessage = formatMessage(font,
-            message1 + message2 + message3);
+        MailMessage mMessage = formatMessage(mailInfo.getFont,
+            mailInfo.getMessage1 + mailInfo.getMessage2 + mailInfo.getMessage3);
         // Send message
-        m.send(mId, subject, mMessage);
+        m.send(mId, mailInfo.getSubject, mMessage);
     }
     // end::buildAndSendMail[]
 
---
+++ src/java/eu/sig/training/ch05/buildandsendmail/MailInfo.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.sig.training.ch05.buildandsendmail;
+
+/**
+ *
+ * @author 16052970
+ */
+public class MailInfo {
+    String firstName;
+    String lastName;
+    String division;
+    String subject;
+    MailFont font;
+    String message1;
+    String message2;
+    String message3;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public MailFont getFont() {
+        return font;
+    }
+
+    public void setFont(MailFont font) {
+        this.font = font;
+    }
+
+    public String getMessage1() {
+        return message1;
+    }
+
+    public void setMessage1(String message1) {
+        this.message1 = message1;
+    }
+
+    public String getMessage2() {
+        return message2;
+    }
+
+    public void setMessage2(String message2) {
+        this.message2 = message2;
+    }
+
+    public String getMessage3() {
+        return message3;
+    }
+
+    public void setMessage3(String message3) {
+        this.message3 = message3;
+    }
+    
+    
+    
+}
